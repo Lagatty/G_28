d49e0770d3cbab3b94e26971f1d09f4d4da65778
Francisco <franciscogatica180@gmail.com>
2025-08-23 18:44:35 -0400

    Clases de reserva

---
+++ src/main/java/com/mycompany/g_28/Reserva_Cabaña.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.g_28;
+
+/**
+ *
+ * @author franc
+ */
+public class Reserva_Cabaña extends ReservaPN {
+    
+}
