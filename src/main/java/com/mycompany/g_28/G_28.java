c9fb7caabbcdb975a489466ea26b8b0611000628
Francisco <franciscogatica180@gmail.com>
2025-08-22 20:06:58 -0400

    Maven project

---
+++ src/main/java/com/mycompany/g_28/G_28.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.g_28;
+
+/**
+ *
+ * @author franc
+ */
+public class G_28 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
